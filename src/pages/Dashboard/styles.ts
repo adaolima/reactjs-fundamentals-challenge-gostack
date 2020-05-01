418bd130117fa0c8986869f3ddb9effd1f01dddd
Adão Lima <adaoolima@gmail.com>
2020-05-01 02:29:30 -0300

    Medias queries

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -9,6 +9,9 @@ export const Container = styled.div`
   max-width: 1120px;
   margin: 0 auto;
   padding: 40px 20px;
+  @media (max-width: 992px) {
+    padding: 40px 0px;
+  }
 `;
 
 export const Title = styled.h1`
@@ -20,7 +23,10 @@ export const CardContainer = styled.section`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 32px;
-  margin-top: -150px;
+  margin-top: -120px;
+  overflow-x: auto;
+  width: 100%;
+  height: 200px;
 `;
 
 export const Card = styled.div`
@@ -28,6 +34,15 @@ export const Card = styled.div`
   padding: 22px 32px;
   border-radius: 5px;
   color: ${({ total }: CardProps): string => (total ? '#fff' : '#363F5F')};
+  display: flex;
+  min-width: 300px;
+  flex-direction: column;
+
+  &:first-child {
+    @media (max-width: 992px) {
+      margin-left: 20px;
+    }
+  }
 
   header {
     display: flex;
@@ -40,10 +55,21 @@ export const Card = styled.div`
   }
 
   h1 {
+    flex: 1;
     margin-top: 14px;
     font-size: 36px;
     font-weight: normal;
     line-height: 54px;
+    display: flex;
+    align-items: flex-end;
+  }
+  span {
+    color: #969cb2;
+    font-weight: 400;
+    font-size: 12px;
+    &.legend-balance {
+      color: #fff;
+    }
   }
 `;
 
@@ -53,14 +79,41 @@ export const TableContainer = styled.section`
   table {
     width: 100%;
     border-spacing: 0 8px;
+    @media (max-width: 992px) {
+      padding: 0px 20px;
+    }
+
+    thead {
+      display: table-caption;
+      @media (max-width: 992px) {
+        display: none;
+      }
+      tr {
+        display: flex;
+      }
+    }
 
     th {
+      flex: 1;
+      display: flex;
       color: #969cb3;
       font-weight: normal;
       padding: 20px 32px;
       text-align: left;
       font-size: 16px;
       line-height: 24px;
+      cursor: pointer;
+      span {
+        margin-right: 15px;
+      }
+      img {
+        transform-origin: center;
+        transition: rotate ease 0.5s;
+        transform: rotate(0deg);
+      }
+      &.active img {
+        transform: rotate(180deg);
+      }
     }
 
     td {
@@ -90,6 +143,50 @@ export const TableContainer = styled.section`
 
     td:last-child {
       border-radius: 0 8px 8px 0;
+    }
+    @media (max-width: 992px) {
+      tr {
+        border-radius: 8px;
+        width: 100%;
+        height: 128px;
+        display: grid;
+        overflow: hidden;
+        padding: 15px 25px;
+        background-color: #fff;
+        td {
+          padding: 0px;
+        }
+        td:first-child,
+        td:last-child {
+          border-radius: 0px;
+        }
+        grid-template-areas:
+          'title title'
+          'price price'
+          'category date';
+        grid-template-rows: 24px 45px auto;
+        grid-template-columns: 1fr 1fr;
+        td:nth-child(1) {
+          grid-area: title;
+          font-size: 14px;
+          line-height: 24px;
+        }
+        td:nth-child(2) {
+          grid-area: price;
+          font-size: 20px;
+          line-height: 32px;
+        }
+        td:nth-child(3) {
+          grid-area: category;
+          font-size: 14px;
+        }
+        td:nth-child(4) {
+          grid-area: date;
+          font-size: 14px;
+          text-align: right;
+        }
+        margin-bottom: 20px;
+      }
     }
   }
 `;
